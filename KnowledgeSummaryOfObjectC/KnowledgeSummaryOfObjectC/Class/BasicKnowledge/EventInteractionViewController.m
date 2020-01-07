ff05ac1aebea20999b6e2298f1f9b9e2e050afe6
张玺 <xiza@ctrip.com>
2020-01-07 10:01:55 +0800

    事件交互练习

--- KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/BasicKnowledge/EventInteractionViewController.m
+++ KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/BasicKnowledge/EventInteractionViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "EventInteractionViewController.h"
-//#import "UIView+BlocksKit.h"
+#import "UIView+BlocksKit.h"
 @interface EventInteractionViewController ()
 @property (nonatomic, strong) UIView *redView;
 @property (nonatomic, strong) UIView *yellowView;
@@ -21,6 +21,7 @@
     
     self.navigationItem.title = @"事件交互";
     
+    [self testOne];
 }
 
 /// 事件点击层级
@@ -28,14 +29,40 @@
     self.redView = [[UIView alloc]init];
     [self.view addSubview:self.redView];
     [self.redView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.left.right.equalTo(self.view);
+        make.left.right.equalTo(self.view);
         make.height.mas_equalTo(300);
+        make.top.equalTo(self.view.mas_top).offset(84);
     }];
     self.redView.backgroundColor = [UIColor redColor];
-//    [self.redView bk_whenTapped:^{
-//        NSLog(@"点击红色模块了");
-//    }];
+    self.redView.userInteractionEnabled = YES;
+    
+    self.yellowView = [[UIView alloc]init];
+    [self.redView addSubview:self.yellowView];
+    [self.yellowView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.redView);
+        make.width.height.mas_equalTo(200);
+    }];
+    self.yellowView.backgroundColor = [UIColor yellowColor];
+    self.yellowView.userInteractionEnabled = YES;
+    
+    
+    self.cyanView = [[UIView alloc]init];
+    [self.yellowView addSubview:self.cyanView];
+    [self.cyanView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.center.equalTo(self.yellowView);
+        make.height.mas_equalTo(100);
+        make.width.mas_equalTo(100);
+    }];
+    self.cyanView.backgroundColor = [UIColor cyanColor];
+    self.cyanView.userInteractionEnabled = YES;
+    
+    
+    [self addEventMethod];
 }
-
+- (void)addEventMethod{
+    [self.yellowView bk_whenTapped:^{
+        NSLog(@"点击了黄色模块了");
+    }];
+}
 
 @end
