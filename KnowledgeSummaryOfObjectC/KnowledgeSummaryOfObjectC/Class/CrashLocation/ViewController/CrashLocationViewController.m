8a67d7d0e760562225056e440bf21d63c1bb902e
xiZhang <zhangxilove2011@163.com>
2021-09-05 22:24:28 +0800

    1111

--- KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CrashLocation/ViewController/CrashLocationViewController.m
+++ KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CrashLocation/ViewController/CrashLocationViewController.m
@@ -20,11 +20,11 @@
     [super viewDidLoad];
     
     
-   // [self test1];
+    [self test1];
 //    [self test2];
     
 //    [self test3];
-    [self test4];
+//    [self test4];
 }
 - (void)viewWillDisappear:(BOOL)animated
 {
