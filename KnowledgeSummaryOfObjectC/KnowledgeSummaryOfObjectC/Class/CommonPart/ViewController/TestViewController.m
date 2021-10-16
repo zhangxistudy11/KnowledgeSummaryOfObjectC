0f5ea6ba8bb5aeba6c1def6ea55dcb8696e661bb
xiZhang <zhangxilove2011@163.com>
2021-10-16 23:30:16 +0800

    完成首页改造功能
    # Conflicts:
    #	KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CommonPart/ViewController/TestViewController.m

--- KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CommonPart/ViewController/TestViewController.m
+++ KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CommonPart/ViewController/TestViewController.m
@@ -7,20 +7,12 @@
 //
 
 #import "TestViewController.h"
-#import <objc/runtime.h>
-#import <malloc/malloc.h>
-#import "SonModel.h"
-#import "People.h"
-#import "NSBundle+YJInfo.h"
-#import "TestUIViewController.h"
-typedef void (^TestBlock)(void);
+
 @interface TestViewController ()<UITableViewDataSource,UITableViewDelegate>
 {
     UITableView  * _tableView;
 }
-@property(nonatomic,copy) TestBlock tBlock;
 
-@property (nonatomic, strong) UIView *redView;
 @end
 
 @implementation TestViewController
@@ -32,7 +24,7 @@ typedef void (^TestBlock)(void);
     self.view.backgroundColor = [UIColor  whiteColor];
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
-    testaa(4);
+
     
     [self  setUpView];
 }
@@ -45,27 +37,16 @@ typedef void (^TestBlock)(void);
     _tableView.dataSource = self;
     [self.view  addSubview:_tableView];
     
-    UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithTitle:@"testALG" style:UIBarButtonItemStylePlain target:self action:@selector(rightclick:)];
-    self.navigationItem.rightBarButtonItem = rightItem;
-    
-    self.redView = [[UIView alloc]init];
-    [self.view addSubview:self.redView];
-    self.redView.backgroundColor = [UIColor redColor];
-//    self.redView.add
-    
 }
 #pragma mark - UITableViewDataSource,UITableViewDelegate
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-  
-    
     return 25;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static  NSString *  CellIdentifier = @"CellIdentifier";
-  
     UITableViewCell  * cell = [tableView  dequeueReusableCellWithIdentifier:CellIdentifier];
     if (!cell) {
         cell = [[UITableViewCell  alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
@@ -80,36 +61,34 @@ typedef void (^TestBlock)(void);
     switch (indexPath.row) {
         case 0:
         {
-            [self testZero];
+            NSArray * arr = nil;
+            [arr count];
+            NSLog(@"ddd%ld",arr.count);
+            NSLog(@"adddaa");
+            NSLog(@"000");
+            [self testOne];
+
         }
             break;
         case 1:
         {
-            // 1.编译时编译器认为testObject是一个NSString对象，这里赋给它一个NSData对象编译器给出黄色类型错误警告，但运行时却指向一个NSData对象
-//            instancetype testObject = [[NSData alloc]init];
-//               // 2.编译器认为testObject是NSString对象，所以允许其调用NSString的方法，这里编译通过无警告和错误
-//               [testObject stringByAppendingString:@"string"];
-//               // 3.但不允许其调用NSData的方法，下面这里编译不通过给出红色报错
-//               [testObject base64EncodedDataWithOptions:NSDataBase64Encoding64CharacterLineLength];
+            NSLog(@"111");
 
-            [self testOne];
-        
+
+            NSLog(@"bfffbbb");
 
         }
             break;
             case 3:
                    {
-                       NSObject * obj = [[NSObject alloc]init];
-                       int a = 10;
-                       NSLog(@"%p %p %p",&obj,obj,a);
-                       NSLog(@"aaa");
-                       NSLog(@"cccgggggg放假啊可怜；fffggggc");
-                       NSLog(@"bbgggggggb");
+                       
+                       NSLog(@"cccdddc");
+
                    }
                        break;
             case 4:
                           {
-                              NSLog(@"333");
+                              
                               NSLog(@"fccbbbc4h555hhh");
 
                           }
@@ -119,45 +98,19 @@ typedef void (^TestBlock)(void);
             break;
     }
 }
-#pragma mark - Test Method
-
-void testaa(int a) {
-    printf("%d",a);
+- (void)testOne{
+    NSMutableArray *array = [[NSMutableArray alloc]initWithArray:@[@(3),@(8),@(5),@(6),@(9),@(11),@(0),@(7)]];
+    NSInteger sun = 11;
+    [self sumtest:array andSum:sun];
 }
-void test() {
-    int a = 10;
-    int b = 20;
-    NSLog(@"%d",a+b);
-}
-- (void)testZero
-{
-  __block  int a = 10;
-    
-    self.tBlock = ^{
-        a = 30;
-    };
-    self.tBlock();
-    NSLog(@"%ld",a);
-}
-#pragma mark - Target Methods
-- (void)rightclick:(UIBarButtonItem *)sender
-{
- 
-    NSArray * arr1 = [NSBundle yj_bundleOwnClassesInfo];
-    NSLog(@"%@",arr1);
-    
-    NSArray * arr2 = [NSBundle yj_bundleAllClassesInfo];
-    NSLog(@"%@",arr2);
-    TestUIViewController *vc = [[TestUIViewController alloc]init];
-    [self.navigationController pushViewController:vc animated:YES];
-   
-
-}
-- (void)testOne {
-    
-    TestModel * tModel = [[TestModel alloc]init];
-    
-  
-    
+- (void)sumtest:(NSMutableArray *)array andSum:(NSInteger)sum{
+    for (int i=0; i<array.count; i++) {
+        for (NSInteger j = array.count-1; j>i; j--) {
+            NSInteger dd = [array[i] integerValue] + [array[j] integerValue];
+            if (dd == sum) {
+                NSLog(@"%@",@[@(i),@(j)]);
+            }
+        }
+    }
 }
 @end
