db6d41d0234d9649cb8ae3890d0d48ad737ae6d9
xiZhang <zhangxilove2011@163.com>
2021-10-23 22:54:53 +0800

    11111

---
+++ KnowledgeSummaryOfObjectC/KnowledgeSummaryOfObjectC/Class/CommonPart/ViewController/TestViewController_BACKUP_3519.m
@@ -0,0 +1,104 @@
+//
+//  TestViewController.m
+//  KnowledgeSummaryOfObjectC
+//
+//  Created by 张玺 on 2020/4/17.
+//  Copyright © 2020 张玺. All rights reserved.
+//
+
+#import "TestViewController.h"
+
+@interface TestViewController ()<UITableViewDataSource,UITableViewDelegate>
+{
+    UITableView  * _tableView;
+}
+
+@end
+
+@implementation TestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.navigationItem.title = @"TestVC";
+    self.view.backgroundColor = [UIColor  whiteColor];
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    
+
+    
+    [self  setUpView];
+}
+
+- (void)setUpView
+{
+    _tableView = [[UITableView  alloc]initWithFrame:CGRectMake(0, 0, self.view.width
+                                                               , self.view.height + 100) style:UITableViewStylePlain];
+    _tableView.delegate =self;
+    _tableView.dataSource = self;
+    [self.view  addSubview:_tableView];
+    
+}
+#pragma mark - UITableViewDataSource,UITableViewDelegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 25;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static  NSString *  CellIdentifier = @"CellIdentifier";
+    UITableViewCell  * cell = [tableView  dequeueReusableCellWithIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell  alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    cell.textLabel.text = [NSString stringWithFormat:@"test-index-%ld",indexPath.row];
+    return cell;
+    
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    switch (indexPath.row) {
+        case 0:
+        {
+<<<<<<< HEAD
+            NSLog(@"000ddd");
+            NSLog(@"ad3333ddssssaaaaa");
+            NSLog(@"000dddggg333");
+            NSLog(@"adddssssaaaaa");
+            NSLog(@"000dddggg33344");
+=======
+            NSLog(@"000dddggg333443333333");
+>>>>>>> 33cdaf1 (11111)
+            NSLog(@"adddssssaaaaa1111");
+            
+        }
+            break;
+        case 1:
+        {
+            NSLog(@"111333ggg");
+
+            NSLog(@"bfffbbb");
+
+        }
+            break;
+            case 3:
+                   {
+                       NSLog(@"aaa");
+                       NSLog(@"cccc");
+                       NSLog(@"bbb");
+                   }
+                       break;
+            case 4:
+                          {
+                              NSLog(@"333");
+                              NSLog(@"fccbbbc4h555hhh");
+
+                          }
+                              break;
+            
+        default:
+            break;
+    }
+}
+@end
